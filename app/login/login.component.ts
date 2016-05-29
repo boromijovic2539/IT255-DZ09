78ca4a9356ac4e2bd4ae959d8323d61f0cb4ebd0
jovanastricic2413 <jovana.stricic@gmail.com>
2016-05-29 14:19:40 +0200

    first commit

---
+++ app/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component, Directive } from 'angular2/core';
+import {ControlGroup} from 'angular2/common';
+@Component({
+ selector: 'Login',
+ templateUrl: 'app/login/login.html'
+})
+export class LoginComponent { 
+}
---
+++ app/signup/signup.component.ts
@@ -0,0 +1,7 @@
+import { Component, Directive } from 'angular2/core';
+@Component({
+ selector: 'Signup',
+ templateUrl: 'app/signup/signup.html'
+})
+export class SignupComponent { 
+}
